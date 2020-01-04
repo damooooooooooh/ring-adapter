d10b4d2b13d478218e8901f10afaf34e7ca8bf9e
Damian <damian@Damians-Mac-4.local>
2020-01-04 00:50:43 +0000

    Update package.sh
    Update missing build items

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 rm -rf node_modules
 npm install --production
 rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+sha256sum README.md manifest.json package.json *.js LICENSE > SHA256SUMS
 find lib -type f -exec sha256sum {} \; >> SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
