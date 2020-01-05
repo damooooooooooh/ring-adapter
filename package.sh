790889bc2e6110a3347979b7cc2f6bdc6bfc981f
Damian <damian@Damians-Mac-4.local>
2020-01-05 10:42:03 +0000

    Package.sh changes - v0.0.5
    Added symbolic links on package.sh that were excluded from SHA256SUMS.  Updated the package description.

--- package.sh
+++ package.sh
@@ -2,11 +2,11 @@
 
 rm -rf node_modules
 npm install --production
-rm -rf ./node_modules/.bin
 rm -f SHA256SUMS
 sha256sum README.md manifest.json package.json *.js LICENSE > SHA256SUMS
 find lib -type f -exec sha256sum {} \; >> SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+find node_modules -type l -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
 cp -r node_modules ./package
